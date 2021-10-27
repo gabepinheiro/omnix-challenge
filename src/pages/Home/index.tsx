807718a4d040c79b0f69791302d5dad93f2be467
Gabriel Pinheiro <gabrielpsilvas@live.com>
2021-10-27 20:08:44 -0300

    add styled-icons

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,4 +1,5 @@
 import { Header } from 'layout/Header'
+import { SearchOutline as SearchIcon } from '@styled-icons/evaicons-outline'
 
 import * as S from './styles'
 
@@ -7,12 +8,14 @@ export function Home () {
     <>
       <Header />
       <S.Content>
-        <S.Title>Seja bem vindo(a) a OmnixFiber</S.Title>
+        <S.Title>Seja bem vindo a OmnixFiber</S.Title>
         <S.SubTitle>Informe seu CEP para a gente separar ofertas especiais para o seu endereço!</S.SubTitle>
 
         <S.Form>
           <S.FormGroup>
-            <S.Label htmlFor='cep'>CEP: </S.Label>
+            <S.Label htmlFor='cep' aria-label='Digite seu CEP' title='Digite seu CEP'>
+              <SearchIcon size={32} />
+            </S.Label>
             <S.Input id='cep' type='text' title='Digite seu CEP' placeholder='Digite seu CEP' />
           </S.FormGroup>
         </S.Form>
